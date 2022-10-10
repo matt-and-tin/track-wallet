1f626fbfc7ac5dcde870054438975798fb8de70e
NFTL0ve <mbrandtk14@gmail.com>
2022-10-10 11:26:48 +0200

    initiation

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("track-wallet JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/track-wallet";
+
+module.exports = MONGO_URI;
